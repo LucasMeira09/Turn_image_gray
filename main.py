162d78ecdf115037df8bca6cc79a96a505ed3931
Lucas de Almeida Meira <lucasdealmeidameira@gmail.com>
2025-07-23 17:12:13 +0200

    add black and white transformation

--- main.py
+++ main.py
@@ -10,14 +10,22 @@ def turn_img_gray(image_path):
 
     # 3. Calculate the weighted grayscale level
     gray = (0.3 * data[:, :, 0] + 0.59 * data[:, :, 1] + 0.11 * data[:, :, 2]).astype(np.uint8)
+    
+    bw = (gray > 127).astype(np.uint8) *255
 
     # 4. Stack the 3 channels R=G=B=gray to reconstruct an RGB image
     gray_rgb = np.stack((gray,) * 3, axis=-1)
-    print(gray_rgb)
+
+    bw_rgb = np.stack((bw,) * 3, axis=-1).astype(np.uint8)
+
     # 5. Create a new image and display it
     gray_img = Image.fromarray(gray_rgb)
+
+    bw_img = Image.fromarray(bw_rgb)
+    
     gray_img.show()
+    bw_img.show()
 
 # Call the function
-image = "descolorir_imagem/car.jpg"
+image = "turn_img_gray/car.jpg"
 turn_img_gray(image)
